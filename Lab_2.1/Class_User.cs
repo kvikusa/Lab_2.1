2864046d01987b85886bc3964d291b5e4087d9a3
Виктория <@KOMPUTER>
2024-10-17 16:13:47 +0500

    Добавьте файлы проекта.

---
+++ Lab_2.1/Class_User.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Lab_2_1
+{
+    public class User
+    {
+        //логические поля
+        bool admin; //пользователь или адинистратор
+        bool active; //активен или нет
+
+        //аксессоры
+        public bool Admin
+        {
+            get => admin;
+            set => admin = value;
+        }
+        public bool Active
+        {
+            get => active;
+            set => active = value;
+        }
+
+        //конструктор
+        public User(bool admin, bool active)
+        {
+            this.admin = admin;
+            this.active = active;
+        }
+        //конструктор копирования
+        public User (User copy)
+        {
+            Admin = copy.Admin;
+            Active = copy.Active;
+        }
+       
+        //метод, вычисляющий отрицания полей
+        public void Negate()
+        {
+            Admin = !Admin;
+            Active = !Active;
+        }
+
+        //перегрузка ToString()
+        public override string ToString()
+        {
+            return $"Пользователь: Админ - {Admin}, Активен - {Active}";
+        }
+    }
+
+    public class PremiumUser : User
+    {
+        int subscriber; //уровень подписки пользователя
+
+        //аксессор
+        public int Subscrider { get => subscriber; set => subscriber = value; } 
+
+        //конструктор
+        public PremiumUser(bool admin, bool active, int sub) : base(admin, active) 
+        {
+            this.subscriber = sub;
+        }
+
+        // Метод для повышения уровня подписки
+        public void Podpiska()
+        {
+            subscriber++;
+        }
+
+        // Метод для проверки доступа к премиум контенту
+        public bool PremiumAccess()
+        {
+            return Active && subscriber > 0;
+        }
+
+        // Переопределение метода ToString()
+        public override string ToString()
+        {
+            return base.ToString() + $", уровень подписки пользователя: {subscriber}";
+        }
+    }
+}
---
+++ Lab_2.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab_2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Тестируем базовый класс User
+            User user1 = new User(true, true);
+            Console.WriteLine("1 пользователь: " + user1);
+            user1.Negate();
+            Console.WriteLine("1 пользователь после отрицания: " + user1);
+
+            // Тестируем конструктор копирования
+            User user2 = new User(user1);
+            Console.WriteLine("2 пользователь (копирует даннные 1-го пользователя): " + user2);
+
+            // Тестируем дочерний класс PremiumUser
+            PremiumUser premiumUser = new PremiumUser(true, true, 1);
+            Console.WriteLine("Пользователь с подпиской: " + premiumUser);
+            premiumUser.Podpiska();
+            Console.WriteLine("Уровень подписки пользователя после повышения: " + premiumUser);
+            Console.WriteLine("Имеет доступ к премиум-контенту: " + premiumUser.PremiumAccess());
+
+            // Тестируем отключение аккаунта
+            premiumUser.Active = false;
+            Console.WriteLine("Отключени аккаунта: " + premiumUser);
+            Console.WriteLine("Имеет доступ к премиум-контенту: " + premiumUser.PremiumAccess());
+        }
+    }
+}
+ 
